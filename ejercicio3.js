6e95a3d47ca9dbfba93093c52d5a2476b1578132
erviti88 <ignacio.erviti@bootcamp-upgrade.com>
2022-11-25 21:52:17 +0100

    Ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,45 @@
+/* Iteración #3: Operadores */
+
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. */
+
+resultado = 10 * 5;
+
+console.log(resultado);
+
+/* 1.2 Divide 10 por 2 y muestra el resultado en un alert. */
+
+resultado = 10 / 2;
+
+console.log(resultado);
+
+/* 1.3 Muestra mediante un alert el resto de dividir 15 por 9. */
+
+resultado = 15 % 9;
+
+console.log(resultado);
+
+/* 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5. */
+
+var y = 10;
+var z = 5;
+
+x = y + z;
+
+console.log("X es igual a ", x);
+
+/* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5. */
+
+y = 10;
+z = 5;
+
+x = y * 5;
+
+console.log("X es igual a ", x);
+
+
+
+
+
+
