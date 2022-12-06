98cbaf673f9a1292cac136cbb0f36c3c6ab63b08
erviti88 <ignacio.erviti@bootcamp-upgrade.com>
2022-12-06 22:34:57 +0100

    Ejercicios Upgrade-Basic-Algorithms terminados

--- ejercicio6.js
+++ ejercicio6.js
@@ -7,8 +7,7 @@ for (let i = 0; i < 10; i++) {
 /* 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
 cuando el resto del numero dividido entre 2 sea 0. */
 
-for (let i = 0; i < 10; i++) {
-    
+for (let i = 0; i < 10; i++) {    
     if ( i % 2 == 0) {
         console.log('Este número SI es par ' + i);
     }
@@ -19,12 +18,12 @@ Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del b
 y cambia el mensaje en la última vuelta a 'Dormido!'. */
 
 for (let i = 0; i <= 10; i++) {
-
     if ( i < 10) {
-        console.log('Intentando dormir');
+        console.log('Intentando dormir: ' + i);
     }
-
     else {
-        console.log('Dormido!');
+        if ( i == 10) {
+            console.log('Dormido!: ' + i);
+        }
     }
 }
